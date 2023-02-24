b8c7c961d34c31912081082940e7ff833ed8e0c4
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-02-24 11:19:43 -0800

    new local repo

--- resume/src/components/banner/Banner.js
+++ resume/src/components/banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <section id='home' className ="w-full h-[800px] pb-20 flex  border-b-[1px]
         border-b-black">
-            Banner
+            Home
         </section>
     );
 
