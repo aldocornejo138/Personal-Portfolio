d291c1443daea567bd8ff49f8dc9740655407f40
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-03-11 09:40:19 -0800

    Updated ProjectCard

--- resume/src/components/projects/Projects.js
+++ resume/src/components/projects/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Title from "../layouts/Title";
-import ProjectCard from "../projects/ProjectCard";
+import { projectOne, projectTwo, projectThree } from "../../assets/index";
+import ProjectsCard from "./ProjectsCard";
 
 const Projects = () => {
   return (
@@ -15,12 +16,16 @@ const Projects = () => {
         />
       </div>
       <div className="grid grid-cols-3 gap-14 ">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        <ProjectsCard
+          title="SOCIAL MEDIA CLONE"
+          des="Lorem, ipsum dolor sit amet consectetur adidossing elit Explicabo qisnsn voludptate sopiente.volutpiotinsus jafrum quidem!"
+          src={projectOne}
+        />
+        <ProjectsCard />
+        <ProjectsCard />
+        <ProjectsCard />
+        <ProjectsCard />
+        <ProjectsCard />
       </div>
     </section>
   );
---
+++ resume/src/components/projects/ProjectsCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProjectsCard = () => {
+  return (
+    <div
+      className="w-full px-12 h-auto py-10 rounded-lg shadow-shadowOne flex flex-col 
+    bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b
+    hover:from-gray-900 hover:gray-900 transition-colors duration-1000"
+    >
+      ProjectCard
+    </div>
+  );
+};
+
+export default ProjectsCard;
