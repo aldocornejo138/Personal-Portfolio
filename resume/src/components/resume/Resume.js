f37652971d93830c176622f8aac2d2bd0bc14fd4
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-03-16 12:30:43 -0700

    Resume Update

--- resume/src/components/resume/Resume.js
+++ resume/src/components/resume/Resume.js
@@ -1,51 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../layouts/Title";
-import ResumeCard from "../resume/ResumeCard";
+import Education from "./Education";
+import Skills from "./Skills";
+import Experience from "./Experience";
+import Achievement from "./Achievement";
 
 const Resume = () => {
+  const [educationData, setEducationData] = useState(true);
+  const [skillData, setSkillData] = useState(false);
+  const [experienceData, setExperienceData] = useState(false);
+  const [achievementData, setAchievementData] = useState(false);
   return (
     <section id="resume" className="w-full py-20 border-b-[1px] border-b-black">
       <div className="flex justify-center items-center text-center">
-        <Title title="5+ Years of experience" des="My Resume" />
+        <Title title="3+ Years of experience" des="My Resume" />
       </div>
       <div>
         <ul className="w-full grid grid-cols-4">
-          <li className="resumeLi">Education </li>
-          <li className="resumeLi">Professional Skill</li>
-          <li className="resumeLi">Experience</li>
-          <li className="resumeLi">Achievements </li>
+          <li
+            onClick={() =>
+              setEducationData(true) &
+              setSkillData(false) &
+              setExperienceData(false) &
+              setAchievementData(false)
+            }
+            className="resumeLi"
+          >
+            Education{" "}
+          </li>
+          <li
+            onClick={() =>
+              setEducationData(false) &
+              setSkillData(true) &
+              setExperienceData(false) &
+              setAchievementData(false)
+            }
+            className="resumeLi"
+          >
+            Professional Skills
+          </li>
+          <li
+            onClick={() =>
+              setEducationData(false) &
+              setSkillData(false) &
+              setExperienceData(true) &
+              setAchievementData(false)
+            }
+            className="resumeLi"
+          >
+            Experience
+          </li>
+          <li
+            onClick={() =>
+              setEducationData(false) &
+              setSkillData(false) &
+              setExperienceData(false) &
+              setAchievementData(true)
+            }
+            className="resumeLi"
+          >
+            Achievements{" "}
+          </li>
         </ul>
       </div>
-      <div className="py-12 font-titleFont">
-        <p className="text-sm text-designColor tracking-[4px]">2015-2021</p>
-        <h2 className="text-4xl font-bold">Education Quality</h2>
-      </div>
-      <div
-        className="mt-14 w-1/2 h-[1000px] border-l-[6px] border-l-black 
-      border-opacity-30 flex flex-col gap-10"
-      >
-        <ResumeCard
-          title="BS-Computer Science"
-          subTitle="California State University San Marcos (2017-2021)"
-          result="3.4/4.0"
-          des="The Training provided by universities in order to prepare people to work 
-          in various sectors of the economy or areas of culture."
-        />
-        <ResumeCard
-          title="AS - Associate in Science"
-          subTitle="Mount San Jacinto College (2015-2017)"
-          result="3.7/4.0"
-          des="Higher education is tertiary education leading to award of an academic 
-          degree. Higher Education, also called post-secondary education"
-        />
-        <ResumeCard
-          title="High School"
-          subTitle="Murrieta Valley High School (2011-2015)"
-          result="3.8/4.0"
-          des="Secondary Education or post-primary education covers two phases on the 
-          International Standard Classification of Education scale"
-        />
-      </div>
+      {educationData && <Education />}
+      {skillData && <Skills />}
+      {experienceData && <Experience />}
+      {Achievement && <Achievement />}
+      {/* <Education /> */}
+      {/* <Skills /> */}
+      {/* <Experience /> */}
+      {/* <Achievement /> */}
     </section>
   );
 };
---
+++ resume/src/components/resume/Skills.js
@@ -0,0 +1,75 @@
+import React from "react";
+import ResumeCard from "./ResumeCard";
+
+const Skills = () => {
+  return (
+    <div className="w-full flex gap-20">
+      <div>
+        <div className="py-12 font-titleFont">
+          <p className="text-sm text-designColor tracking-[4px]">Features</p>
+          <h2 className="text-4xl font-bold">Design Skill</h2>
+        </div>
+        <div
+          className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black 
+      border-opacity-30 flex flex-col gap-10"
+        >
+          <ResumeCard
+            title="BS-Computer Science"
+            subTitle="California State University San Marcos (2017-2021)"
+            result="3.4/4.0"
+            des="The Training provided by universities in order to prepare people to work 
+          in various sectors of the economy or areas of culture."
+          />
+          <ResumeCard
+            title="AS - Associate in Science"
+            subTitle="Mount San Jacinto College (2015-2017)"
+            result="3.7/4.0"
+            des="Higher education is tertiary education leading to award of an academic 
+          degree. Higher Education, also called post-secondary education"
+          />
+          <ResumeCard
+            title="High School"
+            subTitle="Murrieta Valley High School (2011-2015)"
+            result="3.8/4.0"
+            des="Secondary Education or post-primary education covers two phases on the 
+          International Standard Classification of Education scale"
+          />
+        </div>
+      </div>
+      <div>
+        <div className="py-12 font-titleFont">
+          <p className="text-sm text-designColor tracking-[4px]">Features</p>
+          <h2 className="text-4xl font-bold">Development Skill</h2>
+        </div>
+        <div
+          className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black 
+      border-opacity-30 flex flex-col gap-10"
+        >
+          <ResumeCard
+            title="Full Stack Developer"
+            subTitle="Independent FreeLance (2023-Present)"
+            result="USA"
+            des="The Training provided by universities in order to prepare people to work 
+          in various sectors of the economy or areas of culture."
+          />
+          <ResumeCard
+            title="Front End Developer"
+            subTitle="LPL Financial (2022-Present)"
+            result="USA"
+            des="Higher education is tertiary education leading to award of an academic 
+          degree. Higher Education, also called post-secondary education"
+          />
+          <ResumeCard
+            title="Software Developer Engineer in Test"
+            subTitle="TechBee Solutions (2020-2022)"
+            result="USA"
+            des="Secondary Education or post-primary education covers two phases on the 
+          International Standard Classification of Education scale"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
