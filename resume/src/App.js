6c5d392a50ec1a9f5583a1652207a2ec9570e21c
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-03-22 11:56:28 -0700

    Added Testimonial

--- resume/src/App.js
+++ resume/src/App.js
@@ -4,16 +4,18 @@ import Banner from "./components/banner/Banner";
 import Features from "./components/features/Features";
 import Projects from "./components/projects/Projects";
 import Resume from "./components/resume/Resume";
+import Testimonial from "./components/testimonial/Testimonial";
 
 function App() {
   return (
-    <div className="w-full h-auto bg-bodyColor text-lightText">
-      <div className="max-w-screen-2xl mx-auto px-10">
-        <Navbar />
+    <div className="w-full h-auto bg-bodyColor text-lightText px-4">
+      <Navbar />
+      <div className="max-w-screen-2xl mx-auto px-16">
         <Banner />
         <Features />
         <Projects />
         <Resume />
+        <Testimonial />
       </div>
     </div>
   );
