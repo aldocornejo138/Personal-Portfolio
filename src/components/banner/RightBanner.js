552d4c410df71344b394dd2c7a6f5c68a4fcf295
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-04-07 11:06:38 -0700

    took of reveal dependency

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { bannerImg } from "../../assets/index";
-import Fade from "react-reveal/Fade";
 
 const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
-      <Fade bottom delay={1000}>
-        <img
-          className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
-          src={bannerImg}
-          alt="bannerImg"
-        />
-      </Fade>
+      <img
+        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
+        src={bannerImg}
+        alt="bannerImg"
+      />
     </div>
   );
 };
