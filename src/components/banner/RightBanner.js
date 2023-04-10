a16039eea4eadae98961c35de6f3eab12813855f
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-04-10 11:41:36 -0700

    Added a Animation

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -1,14 +1,30 @@
 import React from "react";
 import { bannerImg } from "../../assets/index";
+import { useInView } from "react-intersection-observer";
+import { useRef, useEffect, useState } from "react";
 
 const RightBanner = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+  });
   return (
-    <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
-      <img
-        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
-        src={bannerImg}
-        alt="bannerImg"
-      />
+    <div
+      ref={ref}
+      className=" w-full lgl:w-1/2 flex justify-center items-center relative"
+    >
+      {inView ? (
+        <img
+          className="hiddenReveal w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
+          src={bannerImg}
+          alt="bannerImg"
+        />
+      ) : (
+        <img
+          className="show w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
+          src={bannerImg}
+          alt="bannerImg"
+        />
+      )}
     </div>
   );
 };
