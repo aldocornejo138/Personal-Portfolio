fc4957cbecca38a47a4bf25d740d95d8eb5f4afb
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-11-09 11:10:47 -0800

    update

--- src/components/testimonial/Testimonial.js
+++ src/components/testimonial/Testimonial.js
@@ -118,11 +118,11 @@ const Testimonial = () => {
                     />
                     <div className="w-full flex flex-col justify-end">
                       <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                        Bound - Trolola
+                        Ivan Fountain Service
                       </p>
-                      <h3 className="text-2xl font-bold">Jone Duone Joe</h3>
+                      <h3 className="text-2xl font-bold">Edgar Ivan</h3>
                       <p className="text-base tracking-wide text-gray-500">
-                        Operation Officer
+                        CEO
                       </p>
                     </div>
                   </div>
@@ -132,10 +132,10 @@ const Testimonial = () => {
                       <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
                         <div>
                           <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
-                            Travel Mobile App Design.
+                            Landing Page Design And Built
                           </h3>
                           <p className="text-base text-gray-400 mt-3">
-                            via Upwork - Mar 4, 2015 - Aug 30, 2021 test
+                            via UpWork- Oct 15, 2022 - Aug 30, 2023
                           </p>
                         </div>
                         <div className="text-yellow-500 flex gap-1">
@@ -147,11 +147,16 @@ const Testimonial = () => {
                         </div>
                       </div>
                       <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. A dolorum, eos natus ipsum numquam veniam officia
-                        necessitatibus ratione quos debitis exercitationem
-                        repudiandae facilis id neque nihil accusantium
-                        perspiciatis repellat? Iste.
+                        I can't say enough about the exceptional service
+                        provided by Aldahir Cornejo, the brilliant mind behind
+                        the Fountain maintenance website they created for me.
+                        Their expertise and attention to detail have truly
+                        elevated my business to new heights. Since the launch of
+                        the website, my client base has expanded exponentially.
+                        Not only has it facilitated smoother communication with
+                        existing clients, but it has also attracted a
+                        considerable number of new clients, impressed by the
+                        user-friendly interface and informative content.
                       </p>
                     </div>
                   </div>
@@ -169,11 +174,11 @@ const Testimonial = () => {
                     />
                     <div className="w-full flex flex-col justify-end">
                       <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                        Bound - Trolola
+                        Hecho Por Jenny
                       </p>
-                      <h3 className="text-2xl font-bold">Jone Duone Joe</h3>
+                      <h3 className="text-2xl font-bold">JENNY</h3>
                       <p className="text-base tracking-wide text-gray-500">
-                        Operation Officer
+                        CEO
                       </p>
                     </div>
                   </div>
@@ -183,10 +188,10 @@ const Testimonial = () => {
                       <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
                         <div>
                           <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
-                            Travel Mobile App Design.
+                            Ecommerce Website
                           </h3>
                           <p className="text-base text-gray-400 mt-3">
-                            via Upwork - Mar 4, 2015 - Aug 30, 2021 test
+                            via Upwork - Nov 7, 2022 -2024
                           </p>
                         </div>
                         <div className="text-yellow-500 flex gap-1">
@@ -198,67 +203,19 @@ const Testimonial = () => {
                         </div>
                       </div>
                       <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. A dolorum, eos natus ipsum numquam veniam officia
-                        necessitatibus ratione quos debitis exercitationem
-                        repudiandae facilis id neque nihil accusantium
-                        perspiciatis repellat? Iste.
+                        The website's vibrant and immersive design has
+                        captivated the hearts of my customers, who appreciate
+                        the seamless browsing experience and the stunning
+                        showcase of our local blooms. It has truly brought the
+                        essence of Southern California's natural beauty into the
+                        digital realm, allowing customers to connect with our
+                        region's flora in a unique and enchanting way.
                       </p>
                     </div>
                   </div>
                 </div>
               </div>
               {/* ================ Slider Three ================== */}
-
-              <div className="w-full">
-                <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
-                  <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
-                    <img
-                      className="h-72 md:h-32 lgl:h-72 rounded-lg object-cover"
-                      src={testimonialOne}
-                      alt="testimonialOne"
-                    />
-                    <div className="w-full flex flex-col justify-end">
-                      <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                        Bound - Trolola
-                      </p>
-                      <h3 className="text-2xl font-bold">Jone Duone Joe</h3>
-                      <p className="text-base tracking-wide text-gray-500">
-                        Operation Officer
-                      </p>
-                    </div>
-                  </div>
-                  <div className="w-full lgl:w-[60%] h-full flex flex-col justify-between">
-                    <img className="w-20 lgl:w-32" src={quote} alt="quote" />
-                    <div className="w-full h-[70%] py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
-                      <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
-                        <div>
-                          <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
-                            Travel Mobile App Design.
-                          </h3>
-                          <p className="text-base text-gray-400 mt-3">
-                            via Upwork - Mar 4, 2015 - Aug 30, 2021 test
-                          </p>
-                        </div>
-                        <div className="text-yellow-500 flex gap-1">
-                          <RiStarFill />
-                          <RiStarFill />
-                          <RiStarFill />
-                          <RiStarFill />
-                          <RiStarFill />
-                        </div>
-                      </div>
-                      <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. A dolorum, eos natus ipsum numquam veniam officia
-                        necessitatibus ratione quos debitis exercitationem
-                        repudiandae facilis id neque nihil accusantium
-                        perspiciatis repellat? Iste.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </Slider>
           </div>
         </div>
